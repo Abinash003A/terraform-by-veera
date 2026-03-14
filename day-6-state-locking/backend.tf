0a437ec234e421d84497abb5e331c30f9e5503c3
Abinash <abinashsubudhi213@gmail.com>
2026-03-14 20:36:34 +0530

    state locking using dynamo-db

--- day-6-state-locking/backend.tf
+++ day-6-state-locking/backend.tf
@@ -1,9 +1,13 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-locking-13"
-    key    = "terraform.tfstate"
+    bucket = "my-terraform-state-locking-134"
+    key    = "day-6/terraform.tfstate"
     region = "us-east-1"
-    use_lockfile = true  #dynmodb no longer required for state locking in s3 backend we can use lockfile for state locking in s3 backend
+    dynamodb_table = "terraform-state-lock-dynamo" #use partition key = LockID and sort key = LockID
+
+    #use_lockfile = true
+    #dynmodb no longer required for state locking in s3 backend we can use lockfile for state locking in s3 backend
+    #Enable s3 native state locking using lockfile for state locking in s3 backend
     #terraform version shouid be 1.10 above to use lockfile for state locking in s3 backend
   }
 }
--- day-6-state-locking/main.tf
+++ day-6-state-locking/main.tf
@@ -1,22 +1,6 @@
 resource "aws_vpc" "name" {
-  cidr_block = "10.0.0.0/24"
-  tags = {
-    Name = "dev2-vpc"
-  }
-}
-resource "aws_subnet" "name" {
-  vpc_id     = aws_vpc.name.id
-  cidr_block = "10.0.0.0/26"
+    cidr_block = "10.0.0.0/16"
     tags = {
-        Name = "my-dev2-subnet"
+      Name = "prod"
     }
-}
-resource "aws_instance" "name" {
-    ami           = "ami-02dfbd4ff395f2a1b"
-    instance_type = "t2.micro"
-    subnet_id     = aws_subnet.name.id
-    tags = {
-        Name = "dev2-instance"
-    }
-  
 }
