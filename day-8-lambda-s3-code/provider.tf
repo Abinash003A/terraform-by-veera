1a74f81d53a6fe3544ff71befa71a74b332b5868
Abinash <abinashsubudhi213@gmail.com>
2026-03-18 15:21:04 +0530

    step to instal

--- day-8-lambda-s3-code/provider.tf
+++ day-8-lambda-s3-code/provider.tf
@@ -4,3 +4,6 @@ provider "aws" {
 
 #command tooverwrite zip file
 #zip -r lambda_function.zip lambda_function.py
+#need to install package for zip command in windows
+#choco install zip -y
+#otherwise manual delete the zip file and create a new one
