a05236b6cdef473fa988b3ab6f839d3cd1657448
Abinash <abinashsubudhi213@gmail.com>
2026-03-19 13:00:17 +0530

    understanding modules

---
+++ day-9-module-part/main.tf
@@ -0,0 +1,5 @@
+module "test" {
+    source = "../Day-2"
+    ami_id = "ami-02dfbd4ff395f2a1b"
+    instance_type = "t2.micro"
+}
--- day-9-module-part/provider.tf
+++ day-9-module-part/provider.tf
@@ -1,3 +1,7 @@
-provider "aws" {
+# provider "aws" {
   
-}
+# }
+# we shouldnot define provider in module, we should define provider in root module.
+# 2 providers can cause misconfiguration.
+# Defining provider inside module breaks inheritance from root
+#root provider should not be empty.(atleast region should be defined)
