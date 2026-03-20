45b614dde9ffdff0fc6230f1f07ca0b46861a0bf
Abinash <abinashsubudhi213@gmail.com>
2026-03-20 23:58:01 +0530

    rectified

--- day-11-module-file-in-git/main.tf
+++ day-11-module-file-in-git/main.tf
@@ -1,9 +1,19 @@
 resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr_block
-  tags = var.tags.vpc_tags
+  tags       = var.tags.vpc_tags
+  
 }
-resource "aws_subnet" "my_subnet1_cidr_block" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = var.cidr_block.subnet1_cidr_block
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.subnet_cidr_block.subnet1_cidr_block
   availability_zone = var.availability_zone.subnet1_az
+  tags              = var.tags.subnet1_tags
+  
+}
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.subnet_cidr_block.subnet2_cidr_block
+  availability_zone = var.availability_zone.subnet2_az
+  tags              = var.tags.subnet2_tags
+  
 }
--- day-11-module-file-in-git/variables.tf
+++ day-11-module-file-in-git/variables.tf
@@ -1,22 +1,29 @@
 variable "vpc_cidr_block" {
   description = "CIDR block for the VPC"
-  type        = string 
+  type        = string
+  
+}
+variable "subnet_cidr_block" {
+  description = "CIDR blocks for the subnets"
+  type = object({
+    subnet1_cidr_block = map(string)
+    subnet2_cidr_block = map(string)
+    })
+
 }
 variable "tags" {
-  description = "Tags for the VPC"
-  type        = object({
-    vpc_tags = map(string)
+  description = "Tags for the resources"
+  type = object({
+    vpc_tags     = map(string)
+    subnet1_tags = map(string)
+    subnet2_tags = map(string)
   })
 }
-variable "my_subnet1_cidr_block" {
-  description = "CIDR blocks for the subnets"
-  type        = object({
-    subnet1_cidr_block = string
-  })
-}
-variable "my_subnet1_availability_zone" {
+
+variable "availability_zone" {
   description = "Availability zones for the subnets"
-  type        = object({
-    subnet1_az = string
+  type = object({
+    subnet1_az = map(string)
+    subnet2_az = map(string)
   })
 }
