4630351bce87cb5a48ad91270ddfb0a942fd1ec7
Abinash <abinashsubudhi213@gmail.com>
2026-03-21 00:20:54 +0530

    re-rectified

--- day-11-module-file-in-git/variables.tf
+++ day-11-module-file-in-git/variables.tf
@@ -6,24 +6,24 @@ variable "vpc_cidr_block" {
 variable "subnet_cidr_block" {
   description = "CIDR blocks for the subnets"
   type = object({
-    subnet1_cidr_block = map(string)
-    subnet2_cidr_block = map(string)
+    subnet1_cidr_block = string
+    subnet2_cidr_block = string
     })
 
 }
 variable "tags" {
   description = "Tags for the resources"
   type = object({
-    vpc_tags     = map(string)
-    subnet1_tags = map(string)
-    subnet2_tags = map(string)
+    vpc_tags     = string
+    subnet1_tags = string
+    subnet2_tags = string
   })
 }
 
 variable "availability_zone" {
   description = "Availability zones for the subnets"
   type = object({
-    subnet1_az = map(string)
-    subnet2_az = map(string)
+    subnet1_az = string
+    subnet2_az = string
   })
 }
